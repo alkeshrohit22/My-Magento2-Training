be50a98238cc280ec581a02a68e40be9d32f9d41
alkeshrohit22 <alkesh.rohit@sigmainfo.net>
2023-05-23 16:02:06 +0530

    Block class created

---
+++ app/code/Sigma/FreeShippingBar/Block/Freeshipping.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sigma\FreeShippingBar\Block;
+
+use Magento\Checkout\CustomerData\Cart;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\ScopeInterface;
+
+class Freeshipping extends Template
+{
+    public function __construct(
+        Template\Context $context,
+        private ScopeConfigInterface $scopeConfig,
+        private Cart $cart,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+    }
+
+    public function isFreeShippingEligible(): bool
+    {
+        return $this->scopeConfig->isSetFlag('freeshippingbar/general/is_enable', ScopeInterface::SCOPE_STORE);
+    }
+    public function getFreeShippingAmount(){
+        return $this->scopeConfig->getValue('freeshippingbar/general/free_shipping_amount', ScopeInterface::SCOPE_STORE);
+    }
+}
