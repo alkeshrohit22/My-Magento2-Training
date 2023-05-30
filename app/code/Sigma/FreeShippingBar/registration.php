03b26b7626998bf9d6b3440bcf53f7973ee3ce72
alkeshrohit22 <125466728+alkeshrohit22@users.noreply.github.com>
2023-05-30 12:46:22 +0530

    Delete app/code/Sigma directory

--- app/code/Sigma/FreeShippingBar/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Sigma_FreeShippingBar', __DIR__);
--- app/code/Sigma/OrderComment/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Sigma_OrderComment', __DIR__);
